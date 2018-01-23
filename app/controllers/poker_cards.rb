36606c8d50d040ab807d03a3649ac73c8947b5c6
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2018-01-23 11:56:57 -0700

    Added new and edit for portfolio items

---
+++ app/controllers/poker_cards.rb
@@ -0,0 +1,21 @@
+require "pry"
+
+# my_hash = {}
+
+# cr
+
+# Create a method that returns a random card from a deck of cards. Each card must have a value and a suit.
+
+Deck of Cards has a suit, facecard, value
+
+
+
+
+p deck = {  "hearts" => { },
+            "clubs" => {  },
+            "diamonds" => {  },
+            "spades" => {  }
+
+}
+
+binding.pry
