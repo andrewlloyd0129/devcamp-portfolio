fa443cb033596534fd4713683760bbe29b0be74d
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2018-01-31 11:51:23 -0700

    bye bug and refactoring

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+    before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
     layout "portfolio" 
     def index
         @portfolio_items = Portfolio.all
@@ -26,12 +27,10 @@ class PortfoliosController < ApplicationController
     end
 
     def edit
-        @portfolio_item = Portfolio.find(params[:id]) 
         3.times { @portfolio_item.technologies.build }
     end
 
     def update
-        @portfolio_item = Portfolio.find(params[:id])
 
         respond_to do |format|
             if @portfolio_item.update(portfolio_params)
@@ -43,12 +42,12 @@ class PortfoliosController < ApplicationController
     end
 
     def show
-        @portfolio_item = Portfolio.find(params[:id])
+        
     end
 
     def destroy
         # Perform the Lookup
-        @portfolio_item = Portfolio.find(params[:id])
+
 
         #Destroys the record
         @portfolio_item.destroy
@@ -67,6 +66,9 @@ class PortfoliosController < ApplicationController
                                         :subtitle, 
                                         :body, 
                                         technologies_attributes: [:name])
+    end
+    def set_portfolio_item
+        @portfolio_item = Portfolio.find(params[:id])
     end
 end
 
