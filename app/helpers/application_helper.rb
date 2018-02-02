1f00cebee81055a9806efe7b248bd73f0d879bcf
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2018-02-02 12:19:00 -0700

    added google maps and reworked contact page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,22 @@
 module ApplicationHelper
-  def login_helper
-   if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path) + 
-      "<br>".html_safe +
-      (link_to "Login", new_user_session_path)
+  def login_helper style
+    if current_user.is_a?(GuestUser)
+      (link_to "Register", new_user_registration_path, class: style) +
+      " ".html_safe +
+      (link_to "Login", new_user_session_path, class: style)
     else
-      link_to "Logout", destroy_user_session_path, method: :delete
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
   end
 
-  def source_helper
+  def source_helper(layout_name)
     if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]}"
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
       content_tag(:p, greeting, class: "source-greeting")
-    end 
+    end
   end
 
   def copyright_generator
     ABLViewTool::Renderer.copyright 'Andrew Lloyd', 'All rights reserved'
   end
 end
-
-
