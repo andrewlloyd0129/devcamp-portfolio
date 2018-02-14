ede5c9aae57592c29cc8b039006d02e988b6e285
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2018-02-14 12:03:32 -0700

    Added validations for topics in blogs, added topic form element

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,11 +3,11 @@ class Blog < ApplicationRecord
     extend FriendlyId
     friendly_id :title, use: :slugged
 
-    validates_presence_of :title, :body
+    validates_presence_of :title, :body, :topic_id
 
     has_many :comments, dependent: :destroy
 
-    # belongs_to :topic
+    belongs_to :topic
 
     def self.recent
       order("created_at DESC")
